dcf72fd4067f3c144c86ebad06e68ff3ad710303
YvonneGi <giryvonne@gmail.com>
2019-10-25 14:05:08 +0200

    add search form on navbar

--- awward/views.py
+++ awward/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('awards')
+    return render(request, 'welcome.html')
