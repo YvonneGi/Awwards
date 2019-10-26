aa9bb0fd9ab26ba03f2e8c03bfbe785d0acc7dc6
YvonneGi <giryvonne@gmail.com>
2019-10-26 20:00:21 +0200

    update API view

--- awward/views.py
+++ awward/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import  ProfileMerch
 from .serializer import MerchSerializer
+from rest_framework import status
 
 
 
@@ -67,10 +68,14 @@ def search_results(request):
         return render(request,'search.html',{"message":message,"projects":searched_projects})
 
 class MerchList(APIView):
-    def get(self, request, format=None):
-        all_merch = ProfileMerch.objects.all()
-        serializers = MerchSerializer(all_merch, many=True)
-        return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = MerchSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
    
 
