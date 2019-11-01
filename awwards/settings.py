80d692aa0bc161133de3e0100b3bff8c98a28810
YvonneGi <giryvonne@gmail.com>
2019-11-01 12:36:53 +0200

    deploy to heroku

--- awwards/settings.py
+++ awwards/settings.py
@@ -44,7 +44,7 @@ DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
